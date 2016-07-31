ed37c77360b17b93b10a8ac45e42861018b10ce1
wikelgc <1245788423@qq.com>
2016-07-31 11:50:47 +0800

    update

--- index.js
+++ index.js
@@ -20,37 +20,49 @@ var arr = [wechat_list,contact_list,discover_list,me_list];
 // }
 
 // footer的事件绑定
+// for(var i=0;i<list.length;i++){
+
+// 	if(i==0){
+// 		list[0].onclick = function(){
+// 			display(0);
+// 			console.log(0);
+// 		};
+// 	}
+
+// 	if(i==1){
+// 		list[1].onclick = function(){
+// 			display(1);
+// 			console.log(1);
+// 		};
+// 	}
+
+// 	if(i==2){
+// 		list[2].onclick = function(){
+// 			display(2);
+// 			console.log(2);
+// 		};
+// 	}
+
+// 	if(i==3){
+// 		list[3].onclick = function(){
+// 			display(3);
+// 			console.log(3);
+// 		};
+// 	}
+// 	console.log(i);
+
+// }
+
+//闭包
 for(var i=0;i<list.length;i++){
+	(function(i){
+			return {
+				list[i].onclick = function(){
+					display(i);
+				}
+			}
 
-	if(i==0){
-		list[0].onclick = function(){
-			display(0);
-			console.log(0);
-		};
-	}
-
-	if(i==1){
-		list[1].onclick = function(){
-			display(1);
-			console.log(1);
-		};
-	}
-
-	if(i==2){
-		list[2].onclick = function(){
-			display(2);
-			console.log(2);
-		};
-	}
-
-	if(i==3){
-		list[3].onclick = function(){
-			display(3);
-			console.log(3);
-		};
-	}
-	console.log(i);
-
+	})(i)
 }
 
 
@@ -60,7 +72,7 @@ function display(number){
 	for(var i=0;i<arr.length;i++){
 		arr[i].style.display = "none";
 	}
-	console.log(number);
+	// console.log(number);
 	arr[number].style.display = "block";
 }
 
@@ -73,7 +85,7 @@ var menu = header.getElementsByClassName("menu")[0];
 
 var timer = 0;
 adds.onclick = function(){
-	console.log(15);
+	// console.log(15);
 	// timer++;
 	if(timer%2==0){
 			menu.style.display = "block";
