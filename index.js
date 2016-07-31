b053c5f1c56135810dc786f60c11125903e9e960
wikelgc <1245788423@qq.com>
2016-07-31 11:56:17 +0800

    update

--- index.js
+++ index.js
@@ -12,56 +12,23 @@ var discover_list =document.getElementsByClassName("discover-list")[0];
 var me_list =document.getElementsByClassName("me-list")[0];
 var arr = [wechat_list,contact_list,discover_list,me_list];
 
-// for(var i=0;i<list.length;i++){
-// 	(list[i].onclick = function(){
-// 		// arr[i].style.display =back;
-// 		console.log(this);
-// 	})(i)
-// }
 
-// footer的事件绑定
-// for(var i=0;i<list.length;i++){
 
-// 	if(i==0){
-// 		list[0].onclick = function(){
-// 			display(0);
-// 			console.log(0);
-// 		};
-// 	}
-
-// 	if(i==1){
-// 		list[1].onclick = function(){
-// 			display(1);
-// 			console.log(1);
-// 		};
-// 	}
-
-// 	if(i==2){
-// 		list[2].onclick = function(){
-// 			display(2);
-// 			console.log(2);
-// 		};
-// 	}
-
-// 	if(i==3){
-// 		list[3].onclick = function(){
-// 			display(3);
-// 			console.log(3);
-// 		};
-// 	}
-// 	console.log(i);
-
-// }
-
-//闭包
 for(var i=0;i<list.length;i++){
 	(function(i){
-			return {
-				list[i].onclick = function(){
-					display(i);
-				}
-			}
+		return list[i].onclick=function(){
+			display(i);
+			console.log(1);
 
+			for(j=0;j<list.length;j++){
+				list[j].style.color="#999";
+			}
+			list[i].style.color="#46c018";
+			// list[i].style.backgroundImgae = "url(./icon-menu-"+i+".png)";
+				list[i].style.backgroundImgae = "url(../icon-menu-0)";
+					// list[i].style.backgroundColor = "#333";
+			
+		}
 	})(i)
 }
 
@@ -72,7 +39,7 @@ function display(number){
 	for(var i=0;i<arr.length;i++){
 		arr[i].style.display = "none";
 	}
-	// console.log(number);
+	console.log(number);
 	arr[number].style.display = "block";
 }
 
@@ -85,8 +52,8 @@ var menu = header.getElementsByClassName("menu")[0];
 
 var timer = 0;
 adds.onclick = function(){
-	// console.log(15);
-	// timer++;
+	console.log(15);
+	timer++;
 	if(timer%2==0){
 			menu.style.display = "block";
 	}else{
